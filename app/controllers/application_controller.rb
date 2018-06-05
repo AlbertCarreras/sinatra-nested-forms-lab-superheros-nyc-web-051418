1305bec0a0004c962c63270ff1564d5e9b69bde5
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-05 18:01:00 -0400

    Solved lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, proc { File.join(root, '../views/') }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :super_hero
+  end
 
-
+  post '/teams' do
+    @team = Team.new(params[:team])
+    @super_hero = params[:team][:superheros].map do |ind_hero|
+      SuperHero.new(ind_hero)
+    end
+    @superheros = SuperHero.all
+    erb :team
+  end
 end
